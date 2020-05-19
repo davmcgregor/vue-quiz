404672356456988ce85cbc47882dd48f51de2e79
davidmcgregor <davidjohnmcgregor@gmail.com>
2020-05-19 16:29:35 +0100

    Add grid alayout

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -1,3 +1,18 @@
 <template>
-  <h1>Question Box</h1>
+  <div>
+    <b-jumbotron>
+      <template v-slot:lead>
+        Question
+      </template>
+
+      <hr class="my-4" />
+
+      <p>
+        List of answers
+      </p>
+
+      <b-button variant="primary" href="#">Submit</b-button>
+      <b-button variant="success" href="#">Next</b-button>
+    </b-jumbotron>
+  </div>
 </template>
